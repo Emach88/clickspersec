03ae381c44c9f5b55e1221abb303c7d46365d06a
Emach88 <Eliasjohnstenson@gmail.com>
2025-03-09 14:37:11 -0600

    wip

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 count=0;
 seconds=0;
+url = "https://vast-joey-59273.upstash.io"
 let timerid;
 function timer(){
   seconds=seconds+1;
   if(seconds==5){
     clearInterval(timerid);
     alert(count/seconds);
+    score = count/seconds;
     element = document.getElementById("clicktarget").style.display = 'none';
-    
+    username= document.getElementById("username").value;
+    request = await fetch(url+'/set/'.username+'/'+score,{
+      headers: {"Authorization": "Bearer AeeJAAIjcDExZjg5NWM0OTI1NDE0Y2M1YWNmMjY3NGEyMmMwZTQwMHAxMA"}
+    })
   }
 }
 function countclick(){
